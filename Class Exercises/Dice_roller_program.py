41f76239a2d31dec8b2f6c4b2be82d4e61f17ab7
SRLoveys <147052904+SRLoveys@users.noreply.github.com>
2024-02-19 09:50:04 -0330

    End of Class 8

--- Class Exercises/Dice_roller_program.py
+++ Class Exercises/Dice_roller_program.py
@@ -10,22 +10,21 @@ def main():
     dice = Dice()
     for i in range(num_dice):
         die = Die()
-        dice.add_die(die)
+        dice.addDie(die)
 
     choice = 'y'
     while choice.lower() == 'y':
-        dice.roll_all()
+        dice.rollAll()
 
         print("YOUR ROLL: ", end="")
         for die in dice.list_dice:
-            print(die.get_value(), end=' ')
+            print(die.get_value, end=' ')
         print()
 
         choice = input("Roll again? (y/n): ")
 
-    print("Bye!.")
+    print("Bye!")
 
 
 if __name__ == "__main__":
     main()
-
--- Class Exercises/dice_roller_classes.py
+++ Class Exercises/dice_roller_classes.py
@@ -18,10 +18,10 @@ class Dice:
     def __init__(self):
         self.__list_die = []
 
-    def add_die(self, die):
+    def addDie(self, die):
         self.__list_die.append(die)
 
-    def roll_all(self):
+    def rollAll(self):
         for die in self.__list_die:
             die.roll()
 
