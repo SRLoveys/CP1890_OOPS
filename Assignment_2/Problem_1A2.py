1fb5e5ed2910933cc6efe638215188b1f9459080
SRLoveys <147052904+SRLoveys@users.noreply.github.com>
2024-03-14 01:12:27 -0230

    fix

--- Assignment_2/Problem_1A2.py
+++ Assignment_2/Problem_1A2.py
@@ -9,3 +9,51 @@ class Person:
 
     def get_fullname(self):
         return f"{self.first_name} {self.last_name}"
+
+
+@dataclass
+class Customer(Person):
+    customer_number: str = ""
+
+
+@dataclass
+class Employee(Person):
+    SSN: str = ""
+
+
+def main():
+    print("Customer/Employee Data Entry\n")
+    choice = "y"
+    while choice.lower() == "y":
+        selection = input("Customer or Employee? (c/e): ")
+        if selection.lower() == "c":
+            person = Customer()
+        elif selection.lower() == "e":
+            person = Employee()
+
+        if isinstance(person, Customer):
+            print("\nDATA ENTRY")
+            person.first_name = input("First Name: ").title()
+            person.last_name = input("Last Name: ").title()
+            person.email_address = input("Email: ")
+            person.customer_number = input("Number: ")
+            print("\nCUSTOMER")
+            print(f"Name: {person.get_fullname()}")
+            print(f"Email: {person.email_address}")
+            print(f"Number: {person.customer_number}")
+        elif isinstance(person, Employee):
+            print("\nDATA ENTRY")
+            person.first_name = input("First Name: ").title()
+            person.last_name = input("Last Name: ").title()
+            person.email_address = input("Email: ")
+            person.SSN = input("SSN: ")
+            print("\nEMPLOYEE")
+            print(f"Name: {person.get_fullname()}")
+            print(f"Email: {person.email_address}")
+            print(f"SSN: {person.SSN}")
+        choice = input("\nContinue? (y/n): ")
+    print("\nBye!")
+
+
+if __name__ == "__main__":
+    main()
